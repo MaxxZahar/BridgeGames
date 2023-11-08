8f8133fb817a5619dfd463b2def433dbea3b64d0
MaxxZahar <maxxzahar@rambler.ru>
2023-11-08 19:52:47 +0300

    tournaments

--- server/controllers/tournamentController.js
+++ server/controllers/tournamentController.js
@@ -15,20 +15,44 @@ class TournamentController {
             const tournament = await Tournament.create({ name: name, userId: userId });
             return res.json(tournament);
         } catch (e) {
-            return next(APIError.notAuthorized('Хм авторизован'));
+            return next(APIError.notAuthorized(e.message));
         }
     }
 
-    async getAll() {
-
+    async getAll(req, res) {
+        const tournaments = await Tournament.findAll();
+        res.json(tournaments);
     }
 
-    async getOne() {
-
+    async getOne(req, res, next) {
+        const { id } = req.params;
+        try {
+            const tournament = await Tournament.findOne({ where: { id } });
+            return res.json(tournament);
+        } catch (e) {
+            return next(APIError.badRequest(e.message));
+        }
     }
 
-    async delete() {
-
+    async delete(req, res, next) {
+        const { id } = req.params;
+        try {
+            const token = req.headers.authorization.split(' ')[1];
+            const decoded = jsonwebtoken.verify(token, process.env.SECRET_KEY);
+            const userId = decoded.id;
+            const role = decoded.role;
+            const tournamentToDelete = await Tournament.findOne({ where: { id } });
+            if (!tournamentToDelete) {
+                return next(APIError.badRequest("Такой турнир не существует"));
+            }
+            if (tournamentToDelete.userId !== userId && role !== "ADMIN") {
+                return next(APIError.forbidden("Операция не разрешена"));
+            }
+            await Tournament.destroy({ where: { id } });
+            return res.json({ message: 'success' });
+        } catch (e) {
+            return next(APIError.notAuthorized(e.message));
+        }
     }
 }
 
