90d66e5766989f2053a6f9253c06a5e517027c1b
MaxxZahar <maxxzahar@rambler.ru>
2023-11-08 17:45:45 +0300

    create tournament

--- server/controllers/tournamentController.js
+++ server/controllers/tournamentController.js
@@ -1,6 +1,22 @@
-class TournamentController {
-    async create() {
+const APIError = require('../errors/APIError');
+const { Tournament } = require('../models/models');
+const jsonwebtoken = require('jsonwebtoken');
 
+class TournamentController {
+    async create(req, res, next) {
+        const { name } = req.body;
+        if (!name) {
+            return next(APIError.badRequest('Некорректное название матча'));
+        }
+        try {
+            const token = req.headers.authorization.split(' ')[1];
+            const decoded = jsonwebtoken.verify(token, process.env.SECRET_KEY);
+            const userId = decoded.id;
+            const tournament = await Tournament.create({ name: name, userId: userId });
+            return res.json(tournament);
+        } catch (e) {
+            return next(APIError.notAuthorized('Хм авторизован'));
+        }
     }
 
     async getAll() {
