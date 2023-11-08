bc10b17504466c946ce43f385cc8ca13245f42f9
MaxxZahar <maxxzahar@rambler.ru>
2023-11-08 16:01:45 +0300

    role checking

--- server/routes/playerRouter.js
+++ server/routes/playerRouter.js
@@ -1,13 +1,14 @@
 const Router = require('express');
 const router = new Router();
 const playerController = require('../controllers/playerController');
+const checkRole = require('../middleware/checkRoleMiddleware');
 
 router.post('/', playerController.create);
 
 router.get('/', playerController.getAll);
 router.get('/:id', playerController.getOne);
 
-router.delete('/:id', playerController.delete);
+router.delete('/:id', checkRole('ADMIN'), playerController.delete);
 
 
 module.exports = router;
